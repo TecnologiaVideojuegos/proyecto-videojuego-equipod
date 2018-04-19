003e7f7ce60295da10b4afcedf8c3426ed3b0f8e
Daniel Lopez <37187768+DaniLM30@users.noreply.github.com>
2018-04-19 16:32:35 +0200

    Clases a cambiar

---
+++ Clases/Match.java
@@ -0,0 +1,149 @@
+package FightKnights.Logic;
+
+import FightKnights.Logic.Unit;
+import java.util.ArrayList;
+
+public class Match 
+{
+	private static Match match;
+	
+	private static int turn_count = 0; //Numero de turnos... MAXIMO 20 o algo asi
+	private static int p_turn; // 1 o 2 segun el jugador al ue le toque.
+	
+	private static ArrayList<Unit> p1_deck; //Todas las cartas del jugador 1.
+	private static ArrayList<Unit> p1_hand; //Cartas de la mano del jugador 1.
+	private static ArrayList<Unit> p1_table; //Cartas de la mesa del jugador 1.
+	private static int p1_energy; //Energia del jugador 1.
+    private static int p1_health; //Vida del jugador 1.
+        
+	private static ArrayList<Unit> p2_deck; //Todas las cartas del jugador 2.
+	private static ArrayList<Unit> p2_hand; //Cartas de la mano del jugador 2.
+	private static ArrayList<Unit> p2_table; //Cartas de la mesa del jugador 2.
+	private static int p2_energy; //Energia del jugador 2.
+    private static int p2_health; //Vida del jugador 2.
+	
+	private Match()
+	{
+		//No lo usaremos de momento...
+	}
+	
+	public static Match getMatchInstance()
+	{
+		// ESTO CREA UNA CLASE SI NO ESTA CREADA, Y SI YA LO ESTA DEVUELVE LA QUE YA HAY,
+		// LO UE CONSIGUE QUE SE GUARDEN LOS DATOS MIENTRAS DURA LA PARTIDA ENTERA.
+		
+		if(match == null)
+		{
+			match = new Match();
+		}
+		
+		return match;
+	}
+	
+	// GETTERS Y SETTERS
+	
+	//********************************************
+	public ArrayList<Unit> getP1_deck()
+	{
+		return p1_deck;
+	}
+	public ArrayList<Unit> getP1_hand()
+	{
+		return p1_hand;
+	}
+	public ArrayList<Unit> getP1_table()
+	{
+		return p1_table;
+	}
+	public void setP1_deck(ArrayList<Unit> p1_deck)
+	{
+		this.p1_deck = p1_deck;
+	}
+	public void setP1_hand(ArrayList<Unit> p1_hand)
+	{
+		this.p1_hand = p1_hand;
+	}
+	public void setP1_table(ArrayList<Unit> p1_table)
+	{
+		this.p1_table = p1_table;
+	}
+	public int getP1_energy()
+	{
+		return p1_energy;
+	}
+	public void setP1_energy(int p1_energy)
+	{
+		this.p1_energy = p1_energy;
+	}
+    public int getP1_health() 
+    {
+            return p1_health;
+    }
+    public void setP1_health(int p1_health) 
+        {
+                Match.p1_health = p1_health;
+        }
+        
+	//********************************************
+    
+	public ArrayList<Unit> getP2_deck()
+	{
+		return p2_deck;
+	}
+	public ArrayList<Unit> getP2_hand()
+	{
+		return p2_hand;
+	}
+	public ArrayList<Unit> getP2_table()
+	{
+		return p2_table;
+	}
+	public void setP2_deck(ArrayList<Unit> p2_deck)
+	{
+		this.p2_deck = p2_deck;
+	}
+	public void setP2_hand(ArrayList<Unit> p2_hand)
+	{
+		this.p2_hand = p2_hand;
+	}
+	public void setP2_table(ArrayList<Unit> p2_table)
+	{
+		this.p2_table = p2_table;
+	}
+	public int getP2_energy()
+	{
+		return p2_energy;
+	}
+	public void setP2_energy(int p2_energy)
+	{
+		this.p2_energy = p2_energy;
+	}
+    public int getP2_health() 
+    {
+            return p2_health;
+    }
+    public void setP2_health(int p2_health) 
+    {
+            Match.p2_health = p2_health;
+    }
+    
+	//********************************************
+    
+	public void setTurn_count(int turn_count)
+	{
+		this.turn_count = turn_count;
+	}
+	public int getTurn_count()
+	{
+		return turn_count;
+	}
+	public void setP_turn(int p_turn)
+	{
+		this.p_turn = p_turn;
+	}
+	public int getP_turn()
+	{
+		return p_turn;
+	}
+	
+}
